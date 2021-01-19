3d8d76bc9202e6c308417545062da4a4c5cd80c1
namu <reona7140@naver.com>
2021-01-19 18:51:57 +0900

    solving: beakjoon 1182

--- src/beakjoon/P16197.java
+++ src/beakjoon/P16197.java
@@ -8,24 +8,14 @@ import java.util.StringTokenizer;
 
 public class P16197 {
 
-    /*
-6 2
-..
-..
-..
-o#
-o#
-##
-     */
-
     static int min = Integer.MAX_VALUE;
     static int width;
     static int height;
     static char[][] map;
     static boolean[][] visible;
 
-    static int[] mx = {1, -1, 0, 0};
-    static int[] my = {0, 0, 1, -1};
+    static int[] mx = {0, 0, -1, 1};
+    static int[] my = {1, -1, 0, 0};
 
     public static void main(String[] args) throws IOException {
 
@@ -66,46 +56,44 @@ o#
     }
 
     public static void solution(int index, int firstX, int firstY, int secondX, int secondY) {
-        System.out.println("index = " + index + ", firstX = " + firstX + ", firstY = " + firstY + ", secondX = " + secondX + ", secondY = " + secondY);
-
-        for (char[] chars : map) {
-            System.out.println(Arrays.toString(chars));
-        }
 
         // 10번 보다 많이 눌러야 한다면 return
-        if (index > 10) {
-            return;
-        }
-
-        boolean isFirstDrop = isDrop(firstX, firstY);
-        boolean isSecondDrop = isDrop(secondX, secondY);
-
-        // 만약에 떨어졌다면 재귀 종료
-        if (isFirstDrop || isSecondDrop) {
-
-            // 둘다 떨어졌다면 올바르지 않은 조작
-            if (isFirstDrop && isSecondDrop) {
-                return;
-            }
-
-            min = Math.min(min, index);
-            return;
-        }
-
-        // 만약 이동한 위치의 블럭이 벽이라면 종료
-        if (map[firstY][firstX] == '#' || map[secondY][secondX] == '#') {
-            return;
-        }
-
-        // 이미 방문한 위치라면 종료
-        if (visible[firstY][firstX]) {
+        if (index > 10 || index > min) {
             return;
         }
 
         visible[firstY][firstX] = true;
 
         for (int i = 0; i < 4; i++) {
-            solution(index + 1, firstX + mx[i], firstY + my[i], secondX + mx[i], secondY + my[i]);
+
+            int firstMx = firstX + mx[i];
+            int firstMy = firstY + my[i];
+            int secondMx = secondX + mx[i];
+            int secondMy = secondY + my[i];
+
+            boolean isFirstDrop = isDrop(firstMx, firstMy);
+            boolean isSecondDrop = isDrop(secondMx, secondMy);
+
+            if (isFirstDrop && isSecondDrop) {
+                continue;
+            }
+
+            if (isFirstDrop || isSecondDrop) {
+                min = Math.min(min, index + 1);
+                continue;
+            }
+
+            if (isWall(firstMx, firstMy)) {
+                firstMx = firstX;
+                firstMy = firstY;
+            }
+
+            if (isWall(secondMx, secondMy)) {
+                secondMx = secondX;
+                secondMy = secondY;
+            }
+
+            solution(index + 1, firstMx, firstMy, secondMx, secondMy);
         }
 
         visible[firstY][firstX] = false;
@@ -114,6 +102,10 @@ o#
 
     public static boolean isDrop(int x, int y) {
         return x < 0 || x >= width || y < 0 || y >= height;
+    }
+
+    public static boolean isWall(int x, int y) {
+        return map[y][x] == '#';
     }
 
 }
