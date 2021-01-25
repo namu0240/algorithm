9ea9b4e4a1cefd72842b24edc9c4c5316e813b96
yudonghun <donghun15.yu@samsung.com>
2021-01-25 15:32:02 +0900

    solved: beakjoon 14500

--- src/beakjoon/P14500.java
+++ src/beakjoon/P14500.java
@@ -7,67 +7,184 @@ import java.util.StringTokenizer;
 
 public class P14500 {
 
-    static int n;
-    static int m;
-    static int[][] field;
-    static int max;
-    static boolean[][][] tetrominos = new boolean[][][]{
-            { // ----
-                    {true, true, true, true}
-            },
-            { // ㅁ
-                    {true, true},
-                    {true, true}
-            },
-            { // ㄴ
-                    {true},
-                    {true},
-                    {true},
-                    {true, true}
-            },
-            { // ㄴㄱ
-                    {true},
-                    {true, true},
-                    {false, true}
-            },
-            { // ㅜ
-                    {true, true, true},
-                    {false, true, false}
-            },
-    };
+    static int map[][];
+    static int x, y;
+    static int max = 0;
 
     public static void main(String[] args) throws IOException {
-
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-
-        n = Integer.parseInt(stringTokenizer.nextToken());
-        m = Integer.parseInt(stringTokenizer.nextToken());
-        field = new int[n][m];
-
-        for (int i = 0; i < n; i++) {
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            for (int j = 0; j < m; j++) {
-                field[n][m] = Integer.parseInt(stringTokenizer.nextToken());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String p = br.readLine();
+        StringTokenizer st = new StringTokenizer(p, " ");
+        y = Integer.parseInt(st.nextToken());
+        x = Integer.parseInt(st.nextToken());
+        map = new int[x + 6][y + 6];
+        for (int i = 3; i < y + 3; i++) {
+            p = br.readLine();
+            st = new StringTokenizer(p, " ");
+            for (int j = 3; j < x + 3; j++) {
+                map[j][i] = Integer.parseInt(st.nextToken());
             }
         }
-
-        solution();
-
-    }
-
-    private static void solution() {
-        for (int i = 0; i < field.length; i++) {
-            for (int j = 0; j < field[i].length; j++) {
-                for (boolean[][] tetromino : tetrominos) {
-                    for (int k = 0; k < tetromino.length; k++) {
-                        for (int o = 0; o < tetromino[k].length; o++) {
-
-                        }
-                    }
-                }
+        for (int j = 0; j < x + 3; j++) {
+            for (int i = 0; i < y + 3; i++) {
+                solution(j, i);
             }
         }
+        System.out.println(max);
     }
 
+    static void solution(int a, int b) {
+        int sum = 0;
+        sum += map[a][b];
+        sum += map[a + 1][b];
+        sum += map[a + 2][b];
+        sum += map[a + 3][b];
+        if (max < sum) {
+            max = sum;
+        }
+        sum = 0;
+        sum += map[a][b];
+        sum += map[a][b + 1];
+        sum += map[a][b + 2];
+        sum += map[a][b + 3];
+        if (max < sum) {
+            max = sum;
+        }
+        sum = 0;
+        sum += map[a][b];
+        sum += map[a + 1][b];
+        sum += map[a + 1][b + 1];
+        sum += map[a][b + 1];
+        if (max < sum) {
+            max = sum;
+        }
+        sum = 0;
+        sum += map[a][b];
+        sum += map[a + 1][b];
+        sum += map[a + 2][b];
+        sum += map[a + 2][b + 1];
+        if (max < sum) {
+            max = sum;
+        }
+        sum = 0;
+        sum += map[a][b + 1];
+        sum += map[a + 1][b + 1];
+        sum += map[a + 2][b + 1];
+        sum += map[a + 2][b];
+        if (max < sum) {
+            max = sum;
+        }
+        sum = 0;
+        sum += map[a][b];
+        sum += map[a + 1][b];
+        sum += map[a][b + 1];
+        sum += map[a][b + 2];
+        if (max < sum) {
+            max = sum;
+        }
+        sum = 0;
+        sum += map[a][b];
+        sum += map[a + 1][b + 2];
+        sum += map[a][b + 1];
+        sum += map[a][b + 2];
+        if (max < sum) {
+            max = sum;
+        }
+        sum = 0;
+        sum += map[a][b];
+        sum += map[a][b + 1];
+        sum += map[a + 1][b + 1];
+        sum += map[a + 2][b + 1];
+        if (max < sum) {
+            max = sum;
+        }
+        sum = 0;
+        sum += map[a][b];
+        sum += map[a][b + 1];
+        sum += map[a + 1][b];
+        sum += map[a + 2][b];
+        if (max < sum) {
+            max = sum;
+        }
+        sum = 0;
+        sum += map[a][b + 2];
+        sum += map[a + 1][b + 2];
+        sum += map[a + 1][b + 1];
+        sum += map[a + 1][b];
+        if (max < sum) {
+            max = sum;
+        }
+        sum = 0;
+        sum += map[a + 1][b + 2];
+        sum += map[a + 1][b + 1];
+        sum += map[a + 1][b];
+        sum += map[a][b];
+        if (max < sum) {
+            max = sum;
+        }
+        sum = 0;
+        sum += map[a][b];
+        sum += map[a + 1][b];
+        sum += map[a + 1][b + 1];
+        sum += map[a + 2][b + 1];
+        if (max < sum) {
+            max = sum;
+        }
+        sum = 0;
+        sum += map[a][b + 2];
+        sum += map[a][b + 1];
+        sum += map[a + 1][b + 1];
+        sum += map[a + 1][b];
+        if (max < sum) {
+            max = sum;
+        }
+        sum = 0;
+        sum += map[a + 2][b];
+        sum += map[a + 1][b];
+        sum += map[a + 1][b + 1];
+        sum += map[a][b + 1];
+        if (max < sum) {
+            max = sum;
+        }
+        sum = 0;
+        sum += map[a][b];
+        sum += map[a][b + 1];
+        sum += map[a + 1][b + 1];
+        sum += map[a + 1][b + 2];
+        if (max < sum) {
+            max = sum;
+        }
+        sum = 0;
+        sum += map[a][b];
+        sum += map[a][b + 1];
+        sum += map[a][b + 2];
+        sum += map[a + 1][b + 1];
+        if (max < sum) {
+            max = sum;
+        }
+        sum = 0;
+        sum += map[a][b + 1];
+        sum += map[a + 1][b + 1];
+        sum += map[a + 2][b + 1];
+        sum += map[a + 1][b];
+        if (max < sum) {
+            max = sum;
+        }
+        sum = 0;
+        sum += map[a + 1][b];
+        sum += map[a + 1][b + 1];
+        sum += map[a + 1][b + 2];
+        sum += map[a][b + 1];
+        if (max < sum) {
+            max = sum;
+        }
+        sum = 0;
+        sum += map[a][b];
+        sum += map[a + 1][b];
+        sum += map[a + 2][b];
+        sum += map[a + 1][b + 1];
+        if (max < sum) {
+            max = sum;
+        }
+    }
 }
